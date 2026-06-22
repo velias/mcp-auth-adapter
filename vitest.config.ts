2368d6ef8b3b697b3d0c79bb4b31b44c708eae6f
Vlastimil Elias <velias@redhat.com>
2026-06-22 16:35:26 +0200

    test coverage optimized

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src/**/*.ts'],
+      exclude: ['src/index.ts'],
       reporter: ['text', 'json-summary', 'lcov'],
       thresholds: {
         branches: 80,
